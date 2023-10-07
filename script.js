170e89eab117cf657e16898fa06d9ee17481d5ac
Dasantia89 <143134358+Dasantia89@users.noreply.github.com>
2023-10-07 12:21:02 -0400

    Added the current date in the header

--- script.js
+++ script.js
@@ -5,26 +5,15 @@ var schedule;
 var now;
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+
 now = dayjs().hour();
 getSchedule ();
 displaySchedule();
   // A Save button has been clicked 
   $('.container-lg').on('click', '.saveBtn', saveSchedule);
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
-
-  // TODO: Add code to display the current date in the header of the page.
+  // Display the current date in the header of the page.
+  $('#currentDay').text(dayjs().format('dddd, MMMM DD'));
 });
 
 // On save button click, save entry to local storage
