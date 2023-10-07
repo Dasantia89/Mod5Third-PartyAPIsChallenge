eb2f353f4af424fc1e4a2f3fdb098e0ecc6c1223
Dasantia89 <143134358+Dasantia89@users.noreply.github.com>
2023-10-07 12:14:59 -0400

    Made function to display schedule on screen

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // in the html.
 var schedule;
 var now;
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -11,19 +12,11 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-
+now = dayjs().hour();
+getSchedule ();
+displaySchedule();
   // A Save button has been clicked 
   $('.container-lg').on('click', '.saveBtn', saveSchedule);
-
-
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -46,10 +39,11 @@ function saveSchedule(event) {
   if(entry.val.length >1){
   getSchedule();
   for(var x=0; x<schedule.length; x++){
-    if(schedule[x].hour === entry.hour){
+    if(schedule[x].hour == entry.hour){
       schedule[x].val = entry.val;
     }
   }
+  console.log(schedule);
   localStorage.setItem('schedule',JSON.stringify(schedule));
   }
   else{
@@ -57,16 +51,36 @@ function saveSchedule(event) {
   }
 }
 
+// Retrieve schedule from localstorage or create one with default values if non stored
 function getSchedule (){
   schedule = JSON.parse(localStorage.getItem("schedule")) || [
     {val: '', hour: 9, amPm : '9AM'},
     {val: '', hour: 10, amPm : '10AM'},
     {val: '', hour: 11, amPm : '11AM'},
     {val: '', hour: 12, amPm : '12AM'},
-    {val: '', hour: 1, amPm : '1PM'},
-    {val: '', hour: 2, amPm : '2PM'},
-    {val: '', hour: 3, amPm : '3PM'},
-    {val: '', hour: 4, amPm : '4PM'},
-    {val: '', hour: 5, amPm : '5PM'}
+    {val: '', hour: 13, amPm : '1PM'},
+    {val: '', hour: 14, amPm : '2PM'},
+    {val: '', hour: 15, amPm : '3PM'},
+    {val: '', hour: 16, amPm : '4PM'},
+    {val: '', hour: 17, amPm : '5PM'}
   ];
+}
+
+// Create Elements to display the schedule and append them to the document
+function displaySchedule(){
+  var time; 
+  for(var x = 0; x<schedule.length; x++){
+    if(schedule[x].hour > now){
+      time = 'future';
+    }else if(schedule[x].hour == now){
+      time = 'present';
+    }else{
+      time = 'past';
+    }
+    $(`<div id='${schedule[x].hour}' class = 'row time-block ${time}'>`).appendTo('#holder');
+    $(`<div class="col-2 col-md-1 hour text-center py-3">${schedule[x].amPm}</div>`).appendTo(`#${schedule[x].hour}`);
+    $(`<textarea class="col-8 col-md-10 description" rows="3">${schedule[x].val}</textarea>`).appendTo(`#${schedule[x].hour}`);
+    $('<button class="btn saveBtn col-2 col-md-1" aria-label="save"> <i class="fas fa-save" aria-hidden="true"></i></button>').appendTo(`#${schedule[x].hour}`);
+  
+  }
 }
